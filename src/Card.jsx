ffe9907e18788deb184afbb7523f07cb75b36828
Jaunty <jaunty5899@gmail.com>
2023-07-07 15:16:08 +0530

    final layout

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,12 +4,14 @@ function Card() {
   return (
     <div className="card">
       <span className="title">Sidebar Block</span>
-      <span className="price">$250/mo</span>
+      <span className="price">
+        <b>$250</b>/mo
+      </span>
       <button className="addToCart">add to cart</button>
       <ul className="offerings">
         <li>439,000</li>
         <li>180 x 150</li>
-        <li> Middle Right</li>
+        <li>Middle Right</li>
       </ul>
     </div>
   );
